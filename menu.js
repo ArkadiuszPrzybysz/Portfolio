32a3b5045329f99e452a6a6818985575489dd4e6
ArkadiuszPrzybysz <arekprzybysz2002@gmail.com>
2022-07-15 17:09:58 +0200

    .

--- menu.js
+++ menu.js
@@ -1,11 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav_menu");
 const text = document.querySelector(".header");
+const arrow = document.querySelector(".box");
 
 window.addEventListener('scroll', function(){
   var value = this.window.scrollY;
 
   text.style.top = value * 0.8 + 'px';
+  arrow.style.top = value * 0.1 + 'px';
 })
 
 hamburger.addEventListener("click", () => {
